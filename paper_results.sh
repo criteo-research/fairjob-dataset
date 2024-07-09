91efbe43cdc3363945ef24f1a19f373cd223f644
fpavone <federicopavone01@gmail.com>
2024-07-09 12:12:47 +0200

    cleaned code for final release

--- paper_results.sh
+++ paper_results.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Generate latex tables and plots for data summaries
-python dataset_summary.py
+# Single fit Dummy  
+python example_fit.py --dummy=1 --name=DUMMY
+# Single fit LR unfair
+python example_fit.py --lr=1 --unfair=1 --name=LR_UNFAIR
+# Single fit LR unaware
+python example_fit.py --lr=1 --name=LR_UNAWARE
+# Single LR with fairness penalty
+python example_fit.py --lr_fair=1 --lambda_fair=3.0 --name=LR_FAIR
 
 # Single XGBoost fit for features utility and relative plots
 python example_fit.py --xgb=1 --name=XGB_UNAWARE
 python example_fit.py --xgb=1 --unfair=1 --name=XGB_UNFAIR
-
-# Single Dummy fit 
-python example_fit.py --dummy=1 --name=DUMMY
 
 # Generate LR results with different level of fairness penalty
 python example_simulations_LR.py --lr_fair=1 --lambda_fair=0.1 --name=LR_FAIR
@@ -16,10 +19,7 @@ python example_simulations_LR.py --lr_fair=1 --lambda_fair=0.316 --name=LR_FAIR
 python example_simulations_LR.py --lr_fair=1 --lambda_fair=0.5 --name=LR_FAIR
 python example_simulations_LR.py --lr_fair=1 --lambda_fair=1.0 --name=LR_FAIR
 python example_simulations_LR.py --lr_fair=1 --lambda_fair=10.0 --name=LR_FAIR
-
 # Generate LR results without fairness penalty
 python example_simulations_LR.py --lr=1 --name=LR
 
-
-
-
+# For post-processing of the results see the notebook: dataset_analysis.ipynb
